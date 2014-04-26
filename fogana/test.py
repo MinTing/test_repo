781e46cb7c0f9ceb8fd2629994669b8fde951b24
mintingx <minting.xiao@gmail.com>
2014-04-26 14:37:58 -0700

    Added code dir

--- fogana/test.py
+++
@@ -1,5 +0,0 @@
-for item in tasks:
-    if str(item['assignee'])==str(asana_user):
-        for task in item['task']:
-            if task['name']==title and task['notes']==description:
-                print task
